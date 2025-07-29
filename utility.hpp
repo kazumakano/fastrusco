42938ef77c4adb758d8bcb3cd2bb2e9b63d96167
kazumakano <kazumakano@outlook.jp>
2025-07-29 18:51:37 +0900

    follow one definition rule and apply include guard

--- utility.hpp
+++ utility.hpp
@@ -1,8 +1,9 @@
+#pragma once
 #include <fstream>
 #include <nlohmann/json.hpp>
 
 
-nlohmann::json read_json(const std::string file) {
+inline nlohmann::json read_json(const std::string file) {
   std::ifstream f(file);
   if (!f.is_open()) {
     std::cout << "failed to open " << file << std::endl;
@@ -11,7 +12,7 @@ nlohmann::json read_json(const std::string file) {
   return nlohmann::json::parse(f);
 }
 
-std::string to_str(const nlohmann::json val) {
+inline std::string to_str(const nlohmann::json val) {
   auto val_str = val.dump();
   if (val_str.front() == '"' && val_str.back() == '"') {
     val_str = val_str.substr(1, val_str.length() - 2);
